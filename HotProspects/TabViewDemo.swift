e8a0c86680cc6802a80b1245b5dffbf062341b7e
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-05-29 23:52:43 -0700

    Added a TabView demo

---
+++ HotProspects/TabViewDemo.swift
@@ -0,0 +1,21 @@
+//
+//  TabView.swift
+//  HotProspects
+//
+//  Created by Kenneth Oliver Rathbun on 5/29/24.
+//
+
+import SwiftUI
+
+struct TabViewDemo: View {
+    var body: some View {
+        TabView {
+            Text("Tab 1")
+            Text("Tab 2")
+        }
+    }
+}
+
+#Preview {
+    TabViewDemo()
+}
